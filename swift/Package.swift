e4add7e3123e62272cc8486cbc8f1923d09995ad
NuttyShrimp <jan.lecoutere@hotmail.be>
2026-03-27 22:06:07 +0100

    feat: add system metrics to track memory usage

--- swift/Package.swift
+++ swift/Package.swift
@@ -14,15 +14,19 @@ let package = Package(
         .package(
             url: "https://github.com/apple/swift-configuration.git", from: "1.0.0",
             traits: [.defaults, "CommandLineArguments"]),
-        .package(url: "https://github.com/swift-otel/swift-otel.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-metrics.git", from: "2.4.1"),
+        .package(url: "https://github.com/apple/swift-system-metrics", from: "1.0.0"),
+        .package(url: "https://github.com/swift-server/swift-prometheus.git", from: "2.0.0"),
     ],
     targets: [
         .executableTarget(
             name: "Bap",
             dependencies: [
                 .product(name: "Configuration", package: "swift-configuration"),
+                .product(name: "Metrics", package: "swift-metrics"),
+                .product(name: "Prometheus", package: "swift-prometheus"),
                 .product(name: "Hummingbird", package: "hummingbird"),
-                .product(name: "OTel", package: "swift-otel"),
+                .product(name: "SystemMetrics", package: "swift-system-metrics"),
 
             ],
             path: "Sources/App",
--- swift/Sources/App/Controllers/BenchmarkController.swift
+++ swift/Sources/App/Controllers/BenchmarkController.swift
@@ -3,23 +3,23 @@ import Hummingbird
 struct BenchmarkController: Sendable {
     var benchmark: BenchmarkService
 
-    func addRoutes(to group: RouterGroup<some RequestContext>) {
+    func addRoutes(to group: RouterGroup<MyRequestContext>) {
         group
             .group("/benchmarks")
             .get(use: self.list)
             .get("run/:operation", use: self.run)
     }
 
-    func list(_ request: Request, ctx: some RequestContext) async throws
+    func list(_ request: Request, ctx: MyRequestContext) async throws
         -> BenchmarkOptionsResponse
     {
         return BenchmarkOptionsResponse(
             operations: benchmark.getAvailableOperations(), meta: CreateMeta())
     }
 
-    func run(_ request: Request, ctx: some RequestContext) async throws -> BenchmarkRunResponse {
+    func run(_ request: Request, ctx: MyRequestContext) async throws -> BenchmarkRunResponse {
         let operation = ctx.parameters.get("operation")!
-        var results = try await benchmark.runOperation(for: operation)
+        let results = try await benchmark.runOperation(for: operation)
         return BenchmarkRunResponse(benchmarks: [operation: results], meta: CreateMeta())
     }
 }
