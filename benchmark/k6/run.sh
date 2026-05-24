3ece24184ba73d580f880cc732d1704dcdd5a419
NuttyShrimp <jan.lecoutere@hotmail.be>
2026-05-24 17:34:23 +0200

    feat(k6): fast iteration to speed-up testing + more identification for logging

--- benchmark/k6/run.sh
+++ benchmark/k6/run.sh
@@ -23,7 +23,7 @@ for key in "${!operations[@]}"; do
     echo "  - $value"
     startTime=$(date +%s%3N)
 
-    K6_PROMETHEUS_RW_SERVER_URL=http://localhost:9090/api/v1/write K6_PROMETHEUS_RW_TREND_AS_NATIVE_HISTOGRAM=true K6_PROMETHEUS_RW_PUSH_INTERVAL=1s K6_PROMETHEUS_RW_TREND_STATS="p(95),p(99),min,max,avg" OPERATION=$key SCENARIO=$value k6 run -o experimental-prometheus-rw ./script.ts --tag testid="$key-$value";
+    K6_PROMETHEUS_RW_SERVER_URL=http://localhost:9090/api/v1/write K6_PROMETHEUS_RW_TREND_AS_NATIVE_HISTOGRAM=true K6_PROMETHEUS_RW_PUSH_INTERVAL=1s K6_PROMETHEUS_RW_TREND_STATS="p(95),p(99),min,max,avg" OPERATION=$key SCENARIO=$value k6 run -o experimental-prometheus-rw ./script.ts --tag testid="$RUNTIME-$key-$value";
 
     endTime=$(date +%s%3N)
 
--- benchmark/k6/script.ts
+++ benchmark/k6/script.ts
@@ -26,10 +26,10 @@ export const options = {
         { duration: '4m', target: 2 << 7 }, // 256
         { duration: '5m', target: 2 << 8 }, // 512
         { duration: '6m', target: 2 << 9 }, // 1024
-        { duration: '7m', target: 2 << 10 }, // 2046
-        { duration: '8m', target: 2 << 11 }, // 4096
-        { duration: '9m', target: 2 << 12 }, // 8192
-        { duration: '10m', target: 2 << 13 }, // 16394
+        { duration: '2m', target: 2 << 10 }, // 2046 + reset tim scaling to speed-up tests
+        { duration: '3m', target: 2 << 11 }, // 4096
+        { duration: '4m', target: 2 << 12 }, // 8192
+        { duration: '10m', target: 2 << 13 }, // 16394 + jump to ten because experience learns in this stage alot of tests drop off
         { duration: '10m', target: 2 << 14 }, // 32788
         { duration: '10m', target: 2 << 15 }, // 65536
         { duration: '10m', target: 2 << 16 }, // 131072
@@ -72,7 +72,7 @@ export function setup() {
 }
 
 export default function() {
-  let res = http.get(`http://127.0.0.1:${runtimePort[__ENV.RUNTIME ?? "swift"]}/api/benchmarks/single/${__ENV.OPERATION}/${__ENV.SCENARIO}`, { timeout: 300_000 });
+  let res = http.get(`http://127.0.0.1:${runtimePort[__ENV.RUNTIME ?? "swift"]}/api/benchmarks/single/${__ENV.OPERATION}/${__ENV.SCENARIO}`, { timeout: 120_000, tags: { runtime: __ENV.RUNTIME ?? "swift" } });
   check(res, { "status is in 2xx range": (res) => res.status >= 200 && res.status <= 300 });
   // sleep(1);
 }
